ecd9a6a80cdf2af42a6a558b36dfbaa5fe0c1ecd
YongHee <doctorsean0425@gmail.com>
2025-06-08 20:40:40 +0900

    Delete web directory

--- web/src/utils/utils.ts
+++
@@ -1,6 +0,0 @@
-// 웹뷰 감지기
-
-export const isInsideWebView = (): boolean => {
-    const searchParams = new URLSearchParams(window.location.search);
-    return searchParams.get("from") === "app";
-};
